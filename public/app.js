8ce846b52862bc3a5c6bab8ab6334e98361e2690
Tural Şükürov <tural.shkrov@gmail.com>
2023-08-10 12:13:48 +0400

    Feat: task template

--- public/app.js
+++ public/app.js
@@ -1,1 +1,3 @@
 "use strict";
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
